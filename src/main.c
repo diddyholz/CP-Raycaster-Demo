8cabdaacf9230cb1b6321cadbd264e0fd0441513
diddyholz <sidney.krombholz@gmail.com>
2021-05-15 20:19:48 +0200

    Add all project files

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <cas-sdk/display.h>
+#include <cas-sdk/input/key-input.h>
+#include <cas-sdk/input/touch-input.h>
+
+
+void main()
+{
+  
+}
+
+void main() __attribute__((section(".text.main")));
